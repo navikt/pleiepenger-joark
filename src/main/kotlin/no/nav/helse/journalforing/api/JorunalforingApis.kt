f22cee5c578eb116d65f0d277cd7fe626f5dc091
Maximilian <erik.maximilian.forsman@nav.no>
2019-02-18 16:37:51 +0100

    RequestId generert om ikke satt, og alltid satt som response header

--- src/main/kotlin/no/nav/helse/journalforing/api/JorunalforingApis.kt
+++ src/main/kotlin/no/nav/helse/journalforing/api/JorunalforingApis.kt
@@ -6,6 +6,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.request.ApplicationRequest
 import io.ktor.request.header
 import io.ktor.request.receive
+import io.ktor.response.ApplicationResponse
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
@@ -24,7 +25,7 @@ fun Route.journalforingApis(
 
     post("/v1/journalforing") {
         val melding = call.receive<MeldingV1>()
-        val metadata = MetadataV1(version = 1, correlationId = call.request.getCorrelationId(), requestId = call.request.getRequestId())
+        val metadata = MetadataV1(version = 1, correlationId = call.request.getCorrelationId(), requestId = call.response.getRequestId())
         val journalPostId = journalforingV1Service.journalfor(melding = melding, metaData = metadata)
 
         call.respond(HttpStatusCode.Created, JournalforingResponse(journalPostId = journalPostId.value))
@@ -35,8 +36,8 @@ private fun ApplicationRequest.getCorrelationId(): String {
     return header(HttpHeaders.XCorrelationId) ?: throw ManglerCorrelationId()
 }
 
-private fun ApplicationRequest.getRequestId(): String? {
-    return header(HttpHeaders.XRequestId)
+private fun ApplicationResponse.getRequestId(): String? {
+    return headers[HttpHeaders.XRequestId]
 }
 
 data class JournalforingResponse(val journalPostId: String)
